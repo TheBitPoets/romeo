7003f76f5ebb21adce1bb4c9234837e8d0320706
Antonio <a.caristia@gmail.com>
2026-08-21 17:31:04 +0200

    docs(examples): keep first example beginner-simple

--- examples/first_move.py
+++ examples/first_move.py
@@ -1,15 +1,7 @@
-"""Primo programma Romeo: avanza lentamente, poi fermati."""
-
 from time import sleep
 
 from romeo.easy import forward, stop
 
-
-def main() -> None:
-    forward(0.3)
-    sleep(1)
-    stop()
-
-
-if __name__ == "__main__":
-    main()
+forward(0.3)
+sleep(1)
+stop()
